30ed431ee59aeafc99d851e704486df236bdc471
Srdjan Jovic <sjovic51@gmail.com>
2020-12-23 21:04:49 +0100

    Implement exponentiation for advanced calculator
    - add exponentiation method for the advanced calculator

--- src/main/java/com/jova/calculator/CalculatorAdvanced.java
+++ src/main/java/com/jova/calculator/CalculatorAdvanced.java
@@ -26,6 +26,21 @@ public class CalculatorAdvanced extends Calculator{
         return result.doubleValue();
     }
 
+    private Double calculateIntegerPartExponentiation(Double value, Integer exponent){
+        Integer integerValue = value.intValue();
+        Integer result = integerValue;
+
+        // Special case where both values are 0
+        if (0 == result && 0 == exponent) return 1.0;
+
+        while (1 < exponent){
+            result = result * integerValue;
+            exponent--;
+        }
+
+        return result.doubleValue();
+    }
+
     // Public methods
     public void calculateAdvanced(char action) throws  NotSupportedOperationException, NumberNotInAreaException{
         Double currentValue = getCurrentValue();
@@ -41,7 +56,11 @@ public class CalculatorAdvanced extends Calculator{
                 throw new NumberNotInAreaException();
             }
             result = calculateIntegerPartFactorial(currentValue);
-            setCurrentValue(result);
+        }else{ // When the action is a digit
+            Integer exponent = 48 - (int)action;
+            result = calculateIntegerPartExponentiation(currentValue, exponent);
         }
+
+        setCurrentValue(result);
     }
 }
